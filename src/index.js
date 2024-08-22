141016ddff21a3d1d018ddb2cde46ff9f35470fe
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-22 07:47:11 +0100

    simple react app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import domReady from '@wordpress/dom-ready';
+import { createRoot } from '@wordpress/element';
+
+const App = () => {
+	return (
+		<div>
+			<p>Our React App</p>
+		</div>
+	);
+};
+
+domReady( () => {
+	const root = createRoot(
+		document.getElementById( 'add-media-from-third-party-service' )
+	);
+	root.render( <App /> );
+} );

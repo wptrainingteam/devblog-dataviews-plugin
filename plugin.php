3b45d770591e73d8a071fe70b885ed8b9784b347
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-22 07:20:44 +0100

    plugin creation

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: DevBlog Dataviews Plugin
+ * Description: A companion plugin for a WordPress Developer Blog article.
+ * Version: 1.0.1
+ * Requires at least: 6.1
+ * Requires PHP: 7.4
+ * Author: JuanMa Garrido
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: devblog-dataviews-plugin
+ */
+
+defined( 'ABSPATH' ) || exit;

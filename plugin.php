085220b41b032b4e8fb3f2b31a2dfe3d1fc53b28
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-22 07:28:43 +0100

    Subsection Media Menu

--- plugin.php
+++ plugin.php
@@ -12,3 +12,24 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+add_action( 'admin_menu', 'devblog_dataviews_admin_menu' );
+
+/**
+ * Creates a new Media subsection and set the HTML for it.
+ */
+function devblog_dataviews_admin_menu() {
+	add_media_page(
+		__( 'Add Media from third party service', 'devblog-dataviews-plugin' ),
+		__( 'Add Media from third party service', 'devblog-dataviews-plugin' ),
+		'manage_options',
+		'add-media-from-third-party-service',
+		function () {
+			printf(
+				'<h1>%s</h1><div id="add-media-from-third-party-service"></div>',
+				esc_html__( 'Add Media from third party service', 'devblog-dataviews-plugin' )
+			);
+		}
+	);
+}
+

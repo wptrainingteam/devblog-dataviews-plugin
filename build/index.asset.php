dbd05c4bdb0bb383b6c51da84f0508360bb0e4a0
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-22 13:28:23 +0100

    step1 version completed

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => '6466cc0595c1ebc8dfbf');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-private-apis', 'wp-warning'), 'version' => '3a1e83dd0f9a5bc43209');

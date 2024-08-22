e540d31f3c5972611478b5fd3f4413770eadac50
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-22 11:41:49 +0100

    fields

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { DataViews } from "@wordpress/dataviews";
+import { getTopicsElementsFormat } from "./utils";
 import "./style.scss";
 
 // source "data" definition
@@ -6,7 +7,67 @@ import { dataPhotos } from "./data";
 
 // "defaultLayouts" definition
 // "fields" definition
-
+const fields = [
+  {
+    id: "img_src",
+    label: "Image",
+    render: ({ item }) => (
+      <img alt={item.alt_description} src={item.urls.thumb} />
+    ),
+    enableSorting: false,
+  },
+  {
+    id: "id",
+    label: "ID",
+    enableGlobalSearch: true,
+  },
+  {
+    id: "author",
+    label: "Author",
+    getValue: ({ item }) => `${item.user.first_name} ${item.user.last_name}`,
+    render: ({ item }) => (
+      <a target="_blank" href={item.user.url}>
+        {item.user.first_name} {item.user.last_name}
+      </a>
+    ),
+    enableGlobalSearch: true,
+  },
+  {
+    id: "alt_description",
+    label: "Description",
+    enableGlobalSearch: true,
+  },
+  {
+    id: "topics",
+    label: "Topics",
+    elements: getTopicsElementsFormat(dataPhotos),
+    render: ({ item }) => {
+      return (
+        <div class="topic_photos">
+          {item.topics.map((topic) => (
+            <span class="topic_photo_item">{topic.toUpperCase()}</span>
+          ))}
+        </div>
+      );
+    },
+    filterBy: {
+      operators: ["isAny", "isNone", "isAll", "isNotAll"],
+    },
+    enableSorting: false,
+  },
+  {
+    id: "width",
+    label: "Width",
+    getValue: ({ item }) => parseInt(item.width),
+    enableSorting: true,
+  },
+  {
+    id: "height",
+    label: "Height",
+    getValue: ({ item }) => parseInt(item.height),
+    enableSorting: true,
+  },
+];
 const App = () => {
   // "view" and "setView" definition
   // "processedData" and "paginationInfo" definition

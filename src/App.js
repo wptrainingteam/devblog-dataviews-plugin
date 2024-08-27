9e98323d5c00385724e742328ed2a262b4d2eab4
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-27 15:51:57 +0200

    WordPress JS Coding Standards applied

--- src/App.js
+++ src/App.js
@@ -1,135 +1,138 @@
-import { DataViews, filterSortAndPaginate } from "@wordpress/dataviews";
-import { useState, useMemo } from "@wordpress/element";
-import { getTopicsElementsFormat } from "./utils";
-import "./style.scss";
+import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
+import { useState, useMemo } from '@wordpress/element';
+import { getTopicsElementsFormat } from './utils';
+import './style.scss';
 
 // source "data" definition
-import { dataPhotos } from "./data";
+import { dataPhotos } from './data';
 
 // "defaultLayouts" definition
-const primaryField = "id";
-const mediaField = "img_src";
+const primaryField = 'id';
+const mediaField = 'img_src';
 
 const defaultLayouts = {
-  table: {
-    layout: {
-      primaryField,
-    },
-  },
-  grid: {
-    layout: {
-      primaryField,
-      mediaField,
-    },
-  },
+	table: {
+		layout: {
+			primaryField,
+		},
+	},
+	grid: {
+		layout: {
+			primaryField,
+			mediaField,
+		},
+	},
 };
 
 // "fields" definition
 const fields = [
-  {
-    id: "img_src",
-    label: "Image",
-    render: ({ item }) => (
-      <img alt={item.alt_description} src={item.urls.thumb} />
-    ),
-    enableSorting: false,
-  },
-  {
-    id: "id",
-    label: "ID",
-    enableGlobalSearch: true,
-  },
-  {
-    id: "author",
-    label: "Author",
-    getValue: ({ item }) => `${item.user.first_name} ${item.user.last_name}`,
-    render: ({ item }) => (
-      <a target="_blank" href={item.user.url}>
-        {item.user.first_name} {item.user.last_name}
-      </a>
-    ),
-    enableGlobalSearch: true,
-  },
-  {
-    id: "alt_description",
-    label: "Description",
-    enableGlobalSearch: true,
-  },
-  {
-    id: "topics",
-    label: "Topics",
-    elements: getTopicsElementsFormat(dataPhotos),
-    render: ({ item }) => {
-      return (
-        <div class="topic_photos">
-          {item.topics.map((topic) => (
-            <span class="topic_photo_item">{topic.toUpperCase()}</span>
-          ))}
-        </div>
-      );
-    },
-    filterBy: {
-      operators: ["isAny", "isNone", "isAll", "isNotAll"],
-    },
-    enableSorting: false,
-  },
-  {
-    id: "width",
-    label: "Width",
-    getValue: ({ item }) => parseInt(item.width),
-    enableSorting: true,
-  },
-  {
-    id: "height",
-    label: "Height",
-    getValue: ({ item }) => parseInt(item.height),
-    enableSorting: true,
-  },
+	{
+		id: 'img_src',
+		label: 'Image',
+		render: ( { item } ) => (
+			<img alt={ item.alt_description } src={ item.urls.thumb } />
+		),
+		enableSorting: false,
+	},
+	{
+		id: 'id',
+		label: 'ID',
+		enableGlobalSearch: true,
+	},
+	{
+		id: 'author',
+		label: 'Author',
+		getValue: ( { item } ) =>
+			`${ item.user.first_name } ${ item.user.last_name }`,
+		render: ( { item } ) => (
+			<a target="_blank" href={ item.user.url } rel="noreferrer">
+				{ item.user.first_name } { item.user.last_name }
+			</a>
+		),
+		enableGlobalSearch: true,
+	},
+	{
+		id: 'alt_description',
+		label: 'Description',
+		enableGlobalSearch: true,
+	},
+	{
+		id: 'topics',
+		label: 'Topics',
+		elements: getTopicsElementsFormat( dataPhotos ),
+		render: ( { item } ) => {
+			return (
+				<div className="topic_photos">
+					{ item.topics.map( ( topic ) => (
+						<span key={ topic } className="topic_photo_item">
+							{ topic.toUpperCase() }
+						</span>
+					) ) }
+				</div>
+			);
+		},
+		filterBy: {
+			operators: [ 'isAny', 'isNone', 'isAll', 'isNotAll' ],
+		},
+		enableSorting: false,
+	},
+	{
+		id: 'width',
+		label: 'Width',
+		getValue: ( { item } ) => parseInt( item.width ),
+		enableSorting: true,
+	},
+	{
+		id: 'height',
+		label: 'Height',
+		getValue: ( { item } ) => parseInt( item.height ),
+		enableSorting: true,
+	},
 ];
 const App = () => {
-  // "view" and "setView" definition
-  const [view, setView] = useState({
-    type: "table",
-    perPage: 10,
-    layout: defaultLayouts.table.layout,
-    fields: [
-      "img_src",
-      "id",
-      "alt_description",
-      "author",
-      "topics",
-      "width",
-      "height",
-    ],
-  });
+	// "view" and "setView" definition
+	const [ view, setView ] = useState( {
+		type: 'table',
+		perPage: 10,
+		layout: defaultLayouts.table.layout,
+		fields: [
+			'img_src',
+			'id',
+			'alt_description',
+			'author',
+			'topics',
+			'width',
+			'height',
+		],
+	} );
 
-  // "processedData" and "paginationInfo" definition
-  const { data: processedData, paginationInfo } = useMemo(() => {
-    return filterSortAndPaginate(dataPhotos, view, fields);
-  }, [dataPhotos, view, fields]);
+	// "processedData" and "paginationInfo" definition
+	const { data: processedData, paginationInfo } = useMemo( () => {
+		return filterSortAndPaginate( dataPhotos, view, fields );
+	}, [ view ] );
 
-  // "actions" definition
-  const actions = [
-    {
-      id: "see-original",
-      label: "See Original",
-      callback: ([item]) => {
-        const urlImage = item.urls.raw;
-        window.open(urlImage, "_blank");
-      },
-    },
-  ];
-  return (
-    <DataViews
-      data={processedData}
-      fields={fields}
-      view={view}
-      onChangeView={setView}
-      defaultLayouts={defaultLayouts}
-      actions={actions}
-      paginationInfo={paginationInfo}
-    />
-  );
+	// "actions" definition
+	const actions = [
+		{
+			id: 'see-original',
+			label: 'See Original',
+			callback: ( [ item ] ) => {
+				const urlImage = item.urls.raw;
+				window.open( urlImage, '_blank' );
+			},
+		},
+	];
+	return (
+		<DataViews
+			data={ processedData }
+			fields={ fields }
+			view={ view }
+			onChangeView={ setView }
+			defaultLayouts={ defaultLayouts }
+			actions={ actions }
+			paginationInfo={ paginationInfo }
+		/>
+	);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import domReady from "@wordpress/dom-ready";
-import { createRoot } from "@wordpress/element";
+import domReady from '@wordpress/dom-ready';
+import { createRoot } from '@wordpress/element';
 
-import App from "./App";
+import App from './App';
 
-domReady(() => {
-  const root = createRoot(
-    document.getElementById("add-media-from-third-party-service")
-  );
-  root.render(<App />);
-});
+domReady( () => {
+	const root = createRoot(
+		document.getElementById( 'add-media-from-third-party-service' )
+	);
+	root.render( <App /> );
+} );
